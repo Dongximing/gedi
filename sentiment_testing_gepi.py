1cf3bf3aeab128a098068a81bc4319ae958481dd
Dongximing <dongx34@myumanitoba.ca>
2023-12-15 14:01:47 -0500

    add helf pos helf neg  test

--- sentiment_testing_gepi.py
+++ sentiment_testing_gepi.py
@@ -11,7 +11,7 @@ from transformers import (
 import time
 # ds = load_dataset('imdb', split='test')
 # ds = ds.select(range(2500))
-ds = pd.read_csv('/home/shaowei/sensitive-blocking/sentiment_methods/dataset/sampled_testing_dataset.csv')
+ds = pd.read_csv('/mnt/hdd-data/shaowei/Ximing/sentiment_methods/dataset/sampled_testing_dataset.csv')
 mode = "sentiment"
 # code_desired = "true"
 # code_undesired = "false"
