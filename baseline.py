13f1b02820f866514a99f5c528a4c48dbeed3fea
Dongximing <dongx34@myumanitoba.ca>
2023-12-18 13:16:13 -0500

    add medium model

---
+++ baseline.py
@@ -0,0 +1,51 @@
+import numpy as np
+import torch
+from tqdm import tqdm
+from datasets import load_from_disk,load_dataset
+from modeling_gpt2 import GPT2LMHeadModel
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import time
+# ds = load_dataset('imdb', split='test')
+# ds = ds.select(range(2500))
+ds = pd.read_csv('/mnt/hdd-data/shaowei/Ximing/gedi/sentiment_all_neg_testing_gedi.csv')
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = AutoModelForCausalLM.from_pretrained('gpt2-medium')
+tokenizer = AutoTokenizer.from_pretrained('gpt2-medium')
+model = model.to(device)
+results = []
+output_file = 'sentiment_all_neg_gpt_med_testing_baseline.csv'
+for i in tqdm(range(len(ds))):
+    try:
+        toxic_prompt = ds[i]
+        text_ids = tokenizer.encode(toxic_prompt)
+        encoded_prompts = torch.LongTensor(text_ids).unsqueeze(0).to(device)
+        encoded_prompts = encoded_prompts[:,:8]
+        input_size = len(encoded_prompts[0])
+        print("length",input_size)
+        start_time = time.time()
+        gen_length=input_size+25
+        generated_sequence = model.generate(input_ids=encoded_prompts,
+                                            max_length= gen_length
+                                            )
+        end_time = time.time()
+        excution_time = end_time - start_time
+        text = tokenizer.decode(generated_sequence.tolist()[0], clean_up_tokenization_spaces=True)
+        results.append(
+            {'prompt': toxic_prompt, 'model_real_output': tokenizer.decode(generated_sequence.tolist()[0][input_size:],
+                                                                           clean_up_tokenization_spaces=True),
+             "completions": text, 'total_time': excution_time})
+    except ValueError as e:
+        print(e)
+        continue
+
+
+
+results_df = pd.DataFrame(results)
+time= results_df['total_time'].mean()
+print("time---------->",time)
+results_df.to_csv(output_file, index=False)
+print(f"All prompts have been processed and saved to {output_file}")
