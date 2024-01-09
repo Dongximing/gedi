7623a41d86d2e58c96b1a82cf981833035ce6846
Dongximing <dongx34@myumanitoba.ca>
2024-01-09 13:46:40 -0500

    add  baseline gpt2 medium testing

--- eval_pos.py
+++ eval_pos.py
@@ -32,7 +32,7 @@ def main(test_file, test_column):
     sentence_list['model_score'] = predicted_classes
     print("avg score", predicted_classes.count('positive') / len(predicted_classes))
     #print("avg time",sentence_list['average_cost_time'].mean())
-    #sentence_list.to_csv(test_file, index=False)
+    sentence_list.to_csv(test_file, index=False)
 
 
 
